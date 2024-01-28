77aeac8a6ce2c515a3ccbae6bac6864a8da77321
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2024-01-28 09:51:34 +0100

    Add postscript, FIT image and one image of bootloader

---
+++ mds_external/board/mds_network_player/post-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+OUT_DIR="${BUILD_DIR}/../images/"
+
+FIT_IMAGE_CFG="${BOARD_DIR}/image.its"
+FIT_IMAGE_OUT="${OUT_DIR}/mds_network_player.itb"
+
+echo "FIT_IMAGE_CFG: ${FIT_IMAGE_CFG}"
+echo "FIT_IMAGE_OUT: ${FIT_IMAGE_OUT}"
+
+cp "$FIT_IMAGE_CFG" "$OUT_DIR"
+cd "$OUT_DIR"
+/usr/bin/mkimage -f image.its "${FIT_IMAGE_OUT}"
+
+exit $?
