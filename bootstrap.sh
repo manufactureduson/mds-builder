3ac04f8d3d9bf43e70e6eb73ecf37600d0098c8f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2024-05-29 14:09:13 +0200

    Add bootstrap config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,12 @@
 
 
 ~/Dev/sunxi-tools/sunxi-fel -v uboot out/u-boot-sunxi-with-spl.bin \
-   write-with-progress 0x80000000 out/uImage \
-   write-with-progress 0x80400000 out/rootfs.cpio.uboot \
+   write-with-progress 0x80000000 out/bootstrap/uImage \
+   write-with-progress 0x80500000 out/bootstrap/rootfs.cpio.uboot \
    write-with-progress 0x80FE0000 out/suniv-f1c200s-mds-network-streamer-v1.0.dtb
    
 #   write-with-progress 0x80000000 out/rootfs.ubi
 
 
 
-echo "bootm 0x80000000 0x80400000 0x80FE0000"
+echo "bootm 0x80000000 0x80500000 0x80FE0000"
