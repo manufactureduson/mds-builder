893e1938498031ea1529c1bcf6796623b3389f04
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2024-11-20 07:50:41 +0000

    simplify post-image.sh to be less verbose, and create output directories based on MACHINE name

--- mds_external/board/mds_network_player/post-images.sh
+++ mds_external/board/mds_network_player/post-images.sh
@@ -5,25 +5,12 @@ set -e
 BOARD_DIR="$(dirname $0)"
 OUT_DIR="${BUILD_DIR}/../images/"
 
-FIT_IMAGE_CFG="${BOARD_DIR}/image.its"
-FIT_IMAGE_OUT="${OUT_DIR}/mds_network_player.itb"
+"$BOARD_DIR"/mknandboot.sh "${OUT_DIR}/spi-nand.bin" "${OUT_DIR}"/u-boot-sunxi-with-spl.bin > /dev/null 2>&1
 
-echo "FIT_IMAGE_CFG: ${FIT_IMAGE_CFG}"
-echo "FIT_IMAGE_OUT: ${FIT_IMAGE_OUT}"
+mkdir -p "${BUILD_DIR}"/../../../../out/"$(basename "$CONFIG_DIR")"
+IMAGE_OUT=$(realpath "${BUILD_DIR}"/../../../../out/"$(basename "$CONFIG_DIR")")
 
-cp "$FIT_IMAGE_CFG" "$OUT_DIR"
-cd "$OUT_DIR"
-#/usr/bin/mkimage -f image.its "${FIT_IMAGE_OUT}"
+mkdir -p "$IMAGE_OUT"
+cp "${OUT_DIR}"/* "${IMAGE_OUT}"
 
-"$BOARD_DIR"/mknandboot.sh "${OUT_DIR}/spi-nand.bin" "${OUT_DIR}"/u-boot-sunxi-with-spl.bin 
-
-mkdir -p ${BUILD_DIR}/../../../../out
-
-echo ${OUT_DIR}/
-echo ${BUILD_DIR}/../../out
-echo ${TOP_DIR}
-echo "a ${TOPDIR}"
-
-cp ${OUT_DIR}/* ${BUILD_DIR}/../../../../out
-
-exit $?
+echo "Images are in $IMAGE_OUT"
