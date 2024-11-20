28d0e175e4acc17bc71809da6d564a0134d1e01f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2024-11-20 07:52:55 +0000

    Update esp-hosted to recent version

--- mds_external/local.mk
+++ mds_external/local.mk
@@ -1,3 +1,3 @@
-# ESP_HOSTED_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/esp-hosted
-# LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../../build/linux
+ESP_HOSTED_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/esp-hosted
+LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../../build/linux
 # MESA3D_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/mesa
