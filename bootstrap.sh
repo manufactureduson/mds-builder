2a4e4d23337f347f2dbb0ef37378b149be241b4b
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2024-11-20 08:48:11 +0100

    modify bootstrap.sh with correct patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,31 @@
 #   write-with-progress 0x80000000 out/mds_network_player.itb
 
 
-~/Dev/sunxi-tools/sunxi-fel -v uboot out/u-boot-sunxi-with-spl.bin \
-   write-with-progress 0x80000000 out/bootstrap/uImage \
-   write-with-progress 0x80500000 out/bootstrap/rootfs.cpio.uboot \
-   write-with-progress 0x80FE0000 out/suniv-f1c200s-mds-network-streamer-v1.0.dtb
+if [ "$#" -ne 1 ]; then
+   echo "Usage: $0 <serial_port>"
+   exit 1
+fi
+
+~/Dev/sunxi-tools/sunxi-fel -v uboot out/network_player_bootstrap/u-boot-sunxi-with-spl.bin \
+   write-with-progress 0x80000000 out/network_player_bootstrap/uImage \
+   write-with-progress 0x80500000 out/network_player_bootstrap/rootfs.cpio.uboot \
+   write-with-progress 0x80FE0000 out/network_player_bootstrap/suniv-f1c200s-mds-network-streamer-v1.0.dtb
    
 #   write-with-progress 0x80000000 out/rootfs.ubi
 
 
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
+echo ' ' > $1
 
-echo "bootm 0x80000000 0x80500000 0x80FE0000"
+sleep 1
+echo "bootm 0x80000000 0x80500000 0x80FE0000" > $1
+
